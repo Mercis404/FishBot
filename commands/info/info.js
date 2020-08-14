00521383111c0d6017b1e74813fdb47b98198474
fishfriend0food <64997829+fishfriend0food@users.noreply.github.com>
2020-08-13 23:43:10 -0700

    Update info.js
    displays correct version

--- commands/info/info.js
+++ commands/info/info.js
@@ -1,9 +1,9 @@
 const Discord = require('discord.js');
 module.exports={
-    name: 'version',
+    name: 'info',
     category: 'info',
     run: async(bot, message, args) =>{
-    message.channel.send("I am on version v.1.0.1");
+    message.channel.send("I am on version v.1.0.5");
     }
 }
            
