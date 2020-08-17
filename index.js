bea47d51b183c9f59c8147f0d1854f0b9f9fc4a8
fishfriend0food <64997829+fishfriend0food@users.noreply.github.com>
2020-08-17 13:03:57 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 
 const Discord = require("discord.js");
 const fs = require("fs");
-const config = require(`./config.json`)
+const config = require(`./config.json`);
+const message = require("./events/guild/message");
 const prefix = config.prefix;
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION']});
 bot.prefix = prefix;
@@ -10,6 +11,42 @@ bot.categories = fs.readdirSync('./commands/');
 ["command"].forEach(handler =>{
     require(`./handlers/${handler}`)(bot);
 });
+bot.on('ready', () => {
+    const guild = bot.guilds.cache.get('739620038125813814')
+
+    const userCountChannel = guild.channels.cache.get('744746181325881365')
+    const userCount = guild.members.cache.filter(member => !member.user.bot).size
+    userCountChannel.setName(`User Count: ${userCount}`)
+
+    const botCountChannel = guild.channels.cache.get('744747237975785592')
+    const botCount = guild.members.cache.filter(member => member.user.bot).size
+    botCountChannel.setName(`Bot Count: ${botCount}`)
+
+    const memberCountChannel = guild.channels.cache.get('744748515724623954')
+    const memberCount = guild.members.cache.size
+    memberCountChannel.setName(`Member Count: ${memberCount}`)
+
+    const roleCountChannel = guild.channels.cache.get('744749455307440261')
+    const roleCount = guild.roles.cache.size
+    roleCountChannel.setName(`Role Count: ${roleCount}`)
+
+    const channelCountChannel = guild.channels.cache.get('744750270000660531')
+    const channelCount = guild.channels.cache.size
+    channelCountChannel.setName(`Channels: ${channelCount}`)
+}, 300000)
+bot.on('guildMemberAdd', member => {
+    member.send(`Welcome to the server! Please read the rules.`)
+})
+bot.on("guildMemberAdd", member =>{
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
+    welcomeChannel.send(`Welcome! ${member}, make sure to check the rules at <#739620112029581374> and if you agree to our rules then verify in <#739620391898841149>`)
+    
+})
+
+bot.on("guildMemberRemove", member =>{
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
+    welcomeChannel.send(`Goodbye! ${member}`)
+})
 bot.on("ready", async message => {
     console.log(`${bot.user.username} is now online`);
     await bot.user.setPresence({ game: { name: 'name' }, status: 'dnd' });
