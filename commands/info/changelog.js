fabc20b2faee153a74fbfc88a79377418e24e70c
fishfriend0food <64997829+fishfriend0food@users.noreply.github.com>
2020-08-16 20:05:42 -0700

    Add files via upload

--- commands/info/changelog.js
+++ commands/info/changelog.js
@@ -8,8 +8,9 @@ module.exports={
         .setColor('RANDOM')
         .addFields (
             { name: '**ReAdded:**', value:'channel stats(only works in the Hangout Room server)', inline: false },
-            { name: '**Added:**', value: 'trivia, bread and more secret commands', inline: false},
+            { name: '**Added:**', value: 'welcome and goodbye messages for new server members,trivia, bread and more secret commands', inline: false},
             { name: '**Changes:**', value: 'Info command has been changed to Version, whois registered on featured now displays proper information and is more specific', inline: false },
+            { name: '**Coming soon:**', value: 'More trivia questions, RF(random fact) command, more secret commands'}
         )
         return message.channel.send(changesEmbed)
     }}
