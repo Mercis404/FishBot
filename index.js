2ee220120a360d6648458bc49c4df401f34d46ec
fishfriend0food <64997829+fishfriend0food@users.noreply.github.com>
2020-08-29 23:48:42 -0700

    Add files via upload
    fix

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const config = require(`./config.json`);
 const message = require("./events/guild/message");
 const prefix = config.prefix;
 const Canvas = require('canvas')
-var db = require('quick.db');
 const bot = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 bot.prefix = prefix;
 bot.commands = new Discord.Collection();
@@ -15,49 +14,7 @@ bot.categories = fs.readdirSync('./commands/');
 });
 
 require('http').createServer((req, res) => res.end('Bot is alive!')).listen(3000)
-bot.on('message', async message => {
-    if(message.author.bot) return
-    xp(message)
-    if(message.content.startsWith(`${process.env.PREFIX}rank`)){
-        var user = message.mentions.users.first() || message.author
-        var level = db.get(`guild_${message.guild.id}_level_${user.id}`) || 0
-        level = level.toString()
-        let xp = db.get(`guild_${message.guild.id}_xp_${user.id}`) || 0
-        var xpNeeded = level * 500 + 500
-        let every = db
-        .all()
-        .filter(i => i.ID.startsWith(`guild_${message.guild.id}_xptotal_`))
-        .sort((a, b) => b.data - a.data)
-        var rank = every.map(x => x.ID).indexOf(`guild_${message.guild.id}_xptotal_${user.id}`) + 1
-        rank = rank.toString()
-        let img = await Canvacord.rank({
-            username: user.username,
-            discrim: user.discriminator,
-            status: user.presence.status,
-            currentXP: xp.toString(),
-            neededXP: xpNeeded.toString(),
-            rank,
-            level,
-            avatarURL: user.displayAvatarURL({ format: "png" }),
-            color: "aqua"
-        })
-        return message.channel.send(new Discord.MessageAttachment(img, "rank.png"))
-    }
-})
-function xp(message) {
-    if(message.content.startsWith(process.env.PREFIX)) return
-    const randomNumber = Math.floor(Math.random() *10) + 15
-    db.add(`guild_${message.guild.id}_xp_${message.author.id}`, randomNumber)
-    db.add(`guild_${message.guild.id}_xptotal_${message.guild.id}`, randomNumber)
-    var level = db.get(`guild_${message.guild.id}_level_${message.author.id}`) || 1
-    var xp = db.get(`guild_${message.guild.id}_xp_${message.author.id}`)
-    var xpNeeded = level = 500
-    if(xpNeeded < xp) {
-        var newLevel = db.add(`guild_${message.guild.id}_level_${message.author.id}`, 1)
-        db.subtract(`guild_${message.guild.id}_xp_${message.author.id}`, xpNeeded)
-        message.channel.send(`${message.author}, you have leveled up to level ${newLevel}`)
-    }
-}
+
 bot.on('ready', () => {
   const guild = bot.guilds.cache.get('739620038125813814')
 
