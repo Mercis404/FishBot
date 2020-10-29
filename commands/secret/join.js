353fd98ff10443fad773c692525124fc8f1496a6
Mercis <bellathecutiepie@gmail.com>
2020-10-29 06:11:39 +0000

    broken

--- commands/secret/join.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-module.exports={
-    name: 'join',
-    category: 'secret',
-    run: async(bot, message, args) =>{
-bot.on('message', message => {
-	if (message.content === '!join') {
-		bot.emit('guildMemberAdd', message.member);
-    }})}}
